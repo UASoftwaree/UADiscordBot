862eea673bd859d74a542535642add0585e8bf8a
daulric <57965846+daulric@users.noreply.github.com>
2021-10-28 13:33:11 -0400

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+  return "RoNetwork Status"
+
+def run():
+  app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
